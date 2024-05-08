b72e2f2c234d4c880cfb4136d4983d75c382bfcc
Nathania Saphira <nathaniasaphira@gmail.com>
2024-05-09 03:39:30 +0800

    feat: add database connection using dapper for mysql

---
+++ SportCenter/DataAccess/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportCenter.DataAccess;
+
+public class DatabaseConnection
+{
+    private readonly string _connectionString;
+
+    public DatabaseConnection(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        return new MySqlConnection(_connectionString);
+    }
+}
---
+++ SportCenter/DataAccess/DatabaseHandler.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportCenter.DataAccess;
+
+public class DatabaseHandler
+{
+    private readonly DatabaseConnection _databaseConnection;
+
+    public DatabaseHandler()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+        _databaseConnection = new DatabaseConnection(connectionString);
+    }
+
+    public void InsertNewRow(string column1, string column2)
+    {
+        using (IDbConnection connection = _databaseConnection.CreateConnection())
+        {
+            connection.Open();
+
+            string query = "";
+
+            connection.Execute(query, new { column1, column2 });
+        }
+    }
+
+
+}
