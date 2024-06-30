b60a75e8b23f4061745df6797664c0298114ab1a
Nathania Saphira <nathaniasaphira@gmail.com>
2024-07-01 02:00:54 +0800

    chore: change namespace style

--- SportCenter/Models/Entities/Member.cs
+++ SportCenter/Models/Entities/Member.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace SportCenter.Models.Entities;
 
-namespace SportCenter.Models.Entities
+public class Member
 {
-    public class Member
-    {
-        public static readonly string Table = "members";
+    public static readonly string Table = "members";
 
-        public int Id { get; set; }
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Contact { get; set; }
-    }
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Contact { get; set; }
 }
